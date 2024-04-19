ef824a06d235f1bba08856082aae71b759278368
whastica <drlook92@gmail.com>
2024-04-19 17:39:15 -0500

    Linea de procesos en python

--- procesos.js
+++
@@ -1,8 +0,0 @@
-const nombre = document.getElementById("nombre").value;
-const correo = document.getElementById("correo").value;
-const mensaje = document.getElementById("mensaje").value;
-
-localStorage.setItem("nombre", nombre);
-localStorage.setItem("correo", correo);
-localStorage.setItem("mensaje", mensaje);
-
